5475af357c4d90390875c32dd715d48d4a3a66f4
doguk <dogukanyesilkaya@outlook.com>
2025-03-26 03:58:01 +0300

    some-examples-for-database

---
+++ src/main/resources/examples.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO country VALUES ('de','Europe', 'Germany', '23 Celsius');
+INSERT INTO country VALUES ('uk','Europe', 'United Kingdom', '14 Celsius');
+INSERT INTO country VALUES ('tr','Europe', 'Turkey', '17 Celsius');
+INSERT INTO country VALUES ('bg','Europe', 'Bulgaria', '18 Celsius');
+INSERT INTO country VALUES ('us','North America', 'United States of America', '4 Celsius');
+SELECT * FROM country;
+SELECT * FROM country_log;
