4f69d957859ad0280de5e28c3c92fe10bb8c59ca
doguk <dogukanyesilkaya@outlook.com>
2025-03-26 03:26:27 +0300

    edge-cases-bug-fixing

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -8,67 +8,8 @@ window.onload=async function(){
 async function resetGame(){
     document.getElementById("difficulty").value="moderate";
     setGuesses("moderate")
+    randomTemp=await getRandomTemperature();
     hideElements();
-
-}
-
-function hideElements(){
-    document.getElementById("search").value = "";
-    document.getElementById("randomtemp").value = `Random Temperature: `;
-    document.getElementById("result").textContent = "";
-}
-
-function setGuessLeftText(guessesLeft){
-    document.getElementById("guessesleft").textContent = `You have ${guessesLeft} guesses left`;
-}
-
-function checkGuessAccuracy(guessesLeft,compareInt){
-    if(compareInt === 0){
-        //game won retry popup
-        document.getElementById("popup").getElementsByTagName('p')[0].textContent = "Congrats you won the game!!";
-        document.getElementById("popup").style.display = "block";
-        document.getElementById("overlay").style.display = "block";
-        return;
-    }
-    if(guessesLeft === 0){
-        //game lost retry popup
-        document.getElementById("popup").getElementsByTagName('p')[0].textContent = "You've lost the game.";
-        document.getElementById("popup").style.display = "block";
-        document.getElementById("overlay").style.display = "block";
-    }
-
-}
-
-function setGuesses(diff){
-    switch(diff.toLowerCase()){
-        case "easy":
-            guessesLeft=5;
-            break;
-        case "moderate":
-            guessesLeft=3;
-            break;
-        case "hard":
-            guessesLeft=2;
-            break;
-        case "extreme":
-            guessesLeft=1;
-            break;
-        default:
-            return ;
-    }
-
-    setGuessLeftText(guessesLeft);
-}
-
-async function getRandomTemperature() {
-    try {
-        const response = await fetch("http://localhost:8080/country-randomtemp");
-        const randomtemp = await response.text();
-        document.getElementById("randomtemp").textContent = `Random Temperature: ${randomtemp}`;
-        return randomtemp;
-    } catch (error) {
-        return null;
-    }
 }
 
 document.getElementById("guessButton").addEventListener("click", async function() {
@@ -86,7 +27,7 @@ document.getElementById("guessButton").addEventListener("click", async function(
 
         await response.text().then(temperature => {
             const temperatureInt = parseInt(temperature);
-            const randomtempInt = parseInt(randomtemp);
+            const randomtempInt = parseInt(randomTemp);
             const compareInt = randomtempInt-temperatureInt;
             const compareText = compareInt.toString();
             guessesLeft-=1;
@@ -103,12 +44,12 @@ document.getElementById("guessButton").addEventListener("click", async function(
     }
 });
 
+
 document.getElementById("difficulty").addEventListener("change", async function(event) {
-    randomtemp = await getRandomTemperature();
+    randomTemp = await getRandomTemperature();
     setGuesses(event.target.value);
 })
 
 document.getElementById("retryButton").addEventListener("click", async function() { //randomTemp is not being assigned
-    randomTemp= await getRandomTemperature();
     await resetGame();
 })
